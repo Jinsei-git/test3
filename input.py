f853ee3fe9a284a07a7b488f2f438f6e7b2923a7
AKI6-JPN <akiho.ishibashi.s@gmail.com>
2025-07-09 18:12:56 +0900

    func.1 ユーザーのイベント入力画面
    ポップアップ
    全てテキスト入力

---
+++ input.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+@st.dialog("イベントの詳細を入力してください")
+def vote(item):
+    st.write(f"Why is {item} your favorite?")
+    reason1 = st.text_input("イベント名", key="reason1")
+    reason2 = st.text_input("日時", key="reason2")
+    reason3 = st.text_input("場所", key="reason3")
+    reason4 = st.text_input("申し込み", key="reason4")
+    if st.button("Submit"):
+        st.session_state.vote = {"item": item, "reason1": reason1, "reason2": reason2, "reason3": reason3, "reason4": reason4}
+        st.rerun()
+
+if "vote" not in st.session_state:
+    st.write("イベントの日時を選択してください")
+    if st.button("07/07"):
+        vote("07/07")
+    if st.button("07/10"):
+        vote("07/10")
+else:
+    st.write(f"{st.session_state.vote['reason1']}が、{st.session_state.vote['item']} に、{st.session_state.vote['reason3']}で開催されます。予約は {st.session_state.vote['reason4']}です。")
+
+
