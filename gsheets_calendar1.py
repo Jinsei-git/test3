822b3e49c89a8c121c596270099f59c1ca6e146e
Jinsei <dict25know@gmail.com>
2025-07-29 16:11:35 +0900

    a

--- gsheets_calendar1.py
+++ gsheets_calendar1.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
+from st-gsheets-connection import GSheetsConnection
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read()
